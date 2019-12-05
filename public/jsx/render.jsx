a0e9775b73a01b60e9e635a60e004b9a9ac34d60
Okrekso <semikoseker@gmail.com>
2019-12-05 15:05:23 +0200

    Timer example

--- public/jsx/render.jsx
+++ public/jsx/render.jsx
@@ -7,15 +7,17 @@ import React, {
 } from "react";
 import { render } from "react-dom";
 import Header from "./header/header";
+import TimerBlock from "./timerBlock/timerBlock";
 
 export default function renderSite(to) {
-  render(<Site/>, to);
+  render(<Site />, to);
 }
 
 function Site() {
   return (
     <>
-        <Header/>
+      <Header />
+      <TimerBlock />
     </>
   );
 }
