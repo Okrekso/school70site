2809bcd6021e38a5dfdb4bb1b33f6e251124b541
MykolaCompany <36553905+MykolaCompany@users.noreply.github.com>
2020-04-03 18:29:38 +0300

    Fix bug

--- public/jsx/render.jsx
+++ public/jsx/render.jsx
@@ -1,35 +1,8 @@
-import React, {
-  useState,
-  useContext,
-  useEffect,
-  useCallback,
-  useMemo
-} from "react";
+import React from "react";
 import { render } from "react-dom";
 import App from './App';
-import Header from "./header/header";
-import TimerBlock from "./timerBlock/timerBlock";
-
 import "../scss/global.scss";
-
-import EventBlock from "./eventBlockComponent/eventBlock";
-import ProposalBlock from "./proposalBlockComponent/proposalBlock";
-
 
 export default function renderSite(to) {
   render(<App />, to);
-}
-
-function Site() {
-  return (
-    <>
-    {/* <text>lol</text> */}
-    <App/>
-    {/* <Header/> */}
-    </>
-    <div id="content">
-      <ProposalBlock/>
-      <EventBlock/>  
-    </div>
-  );
 }
